f0301a829462da1a76ff767b64b4608788acc287
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-08 12:21:38 +0200

    api

---
+++ api/api.js
@@ -0,0 +1,14 @@
+const express = require("express") // call l'install
+const app = express()               // call function
+
+
+
+                // structure des 3 app //
+
+app.get()
+
+app.post("/sign-up", (req, res) => {   // req ds la db
+    
+})  
+
+app.post()
