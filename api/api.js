251b9040fbbc84784451c5cfc0e2de70b59f40e5
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-09 16:51:09 +0200

    signIn

--- api/api.js
+++ api/api.js
@@ -10,10 +10,12 @@ app.use(express.urlencoded({extended: false}))
                 // structure des 3 app //
 
 
-// app.get()
+app.get()
+
 
 app.post("/sign-up", (req, res) => {   // req ds la db
   const name = req.body.name
+  const last = req.body.last
   const email = req.body.email
   const password =  req.body.password
   const image = req.body.image
@@ -21,7 +23,7 @@ app.post("/sign-up", (req, res) => {   // req ds la db
 
   bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) throw err
-    let insertUser = `INSERT INTO users (name, email, password, image) VALUES ("${name}", "${email}", "${hash}", "${image}")`;
+    let insertUser = `INSERT INTO users (name,last, email, password, image) VALUES ("${name}","${last}", "${email}", "${hash}", "${image}")`;
     console.log(insertUser)
 
     conn.query(insertUser, (err, result) => {
@@ -36,7 +38,33 @@ app.post("/sign-up", (req, res) => {   // req ds la db
   
 })  
 
-// app.post()
+ app.post("/sign-in", (req, res) => {
+    const email = req.body.email
+    const password =  req.body.password
+    console.log(email, password);
+
+    conn.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, function(err, result){
+        if (err) throw err
+        console.log(result[0]);
+
+        if (result.length < 1){               
+            res.status(401).send("Incorrect")     // wrong or missing email from db => 401
+        } else {
+            let hashed = result[0].password      // TextRow in terminal = Result
+            bcrypt.compare(password, hashed, function (err, result){
+                if (result) {
+
+                    res.status(200).send("Vous êtes connecté !")
+                
+                } else {
+
+                    res.status(401).send("Mauvais mot de passe :(")
+                }
+            })
+        }
+    })
+ })
+
 
 
 
