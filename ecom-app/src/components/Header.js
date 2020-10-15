8dbac1a22f10e4702973f72572dbcecbeb1791ce
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-15 17:43:54 +0200

    react

--- ecom-app/src/components/Header.js
+++ ecom-app/src/components/Header.js
@@ -2,6 +2,10 @@ import React from "react"; // add import in every components !
 import Nav from "react-bootstrap/Nav";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
+
 
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
@@ -14,7 +18,8 @@ class Header extends React.Component {
     return (
       <div>
         <Router>
-          <Nav className="bg-dark pt-3 pb-3" activeKey="/home">
+          <Nav className="bg-dark pt-3 pb-3" activeKey="/home" >
+
             <Nav.Item>
               <Nav.Link eventKey="disabled" disabled>
                 E-commerce
@@ -23,10 +28,35 @@ class Header extends React.Component {
               <Nav.Item>
                 <Nav.Link href="/sign-up">Sign-up</Nav.Link>
               </Nav.Item>
+
               <Nav.Item>
                 <Nav.Link href="/sign-in">Sign-in</Nav.Link>
               </Nav.Item>
+
+              <Nav.Item>
+                <Nav.Link href="/products">Products</Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Nav.Link href="/create-product">Create product</Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item>
+              
+          
+        <Col xs={2} md={1}>
+            < Image src={ require('../image/pistol.jpeg') } alt="mamie" height={45} width={45} roundedCircle />         
+          </Col>
+        
+        
+          </Nav.Item>
+
+              <Nav.Item>
+                <Button variant="outline-danger">Sign out</Button>{' '}
+              </Nav.Item>
+            
           </Nav>
+
                                              {/* import */}
           <Switch>
             <Route path="/sign-in">
---
+++ ecom-app/src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';    // rcc (shortcut)
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProductList;
