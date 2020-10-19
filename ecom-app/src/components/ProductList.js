368331b472bcd9ec24964843cd1cb72ce3a4ad2c
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-19 17:03:29 +0200

    react

--- ecom-app/src/components/ProductList.js
+++ ecom-app/src/components/ProductList.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import {getProducts} from '../actions/productsActions'
 
 
-
 class ProductList extends Component {
     constructor() {
         super();
@@ -26,27 +25,28 @@ class ProductList extends Component {
         this.props.getProducts(products)
         console.log(products)
     }
-    componentDidMount() {
-        this.getProducts()
-      }
+  
     render() {
+        this.getProducts()
         return (
             <div>
                 <h1>ProductList</h1> 
             
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="https://images-na.ssl-images-amazon.com/images/I/81yfpqyMXbL._AC_SL1500_.jpg" />
+                {this.props.products.map((card) => (
+                <Card style={{ width: '18rem' }} key={card.id} >
+                    
+                    <Card.Img variant="top" src={card.image} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>{card.titre}</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            {card.description}
                         </Card.Text>
                     </Card.Body>
                 </Card>
-
+                )
+            )}
             </div>
+        
         );
     }
 }
--- ecom-app/src/components/ProductPage.js
+++ ecom-app/src/components/ProductPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class ProductPage extends Component {
     render() {
         return (
