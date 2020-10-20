2614e45e33503b95650421ed90076ba234a32fe6
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-20 14:26:20 +0200

    react

--- ecom-app/src/components/ProductPage.js
+++ ecom-app/src/components/ProductPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
-
+import { connect } from 'react-redux';
+import {getProducts} from '../actions/productsActions'
 
 
 
@@ -11,22 +12,37 @@ class ProductPage extends Component {
             id: null
         }
     }
-    componentDidMount() {
-        const id = this.props.match.params.id;
-        this.getId(id);
-        console.log(id)
-    }
-    getId = (id) => {
-        this.setState({id: id})
-    }
+
     render() {
+        
+        const id = this.props.match.params.id;
+    
         return (
             <div>
-                <h1>Product number: {this.state.id}</h1>
+            <h1>Titre {this.props.products[this.props.match.params.id].titre || "lol"}</h1>
+            <img src="/"/>
+            <p>Description du produit</p>
+            <p>Prix</p>
+            <p>Stock</p>
+
             </div>
         );
     }
 }
 
+function mapStateToProps(state) { //Accéder aux données de notre store dans les props
+    return {
+        token: state.isLogged.token,
+        products: state.products
 
-export default withRouter(ProductPage);
+    };
+  }
+  
+  const mapDispatchToProps = { //Permettre de modifier les données par l'appel des actions en les appelant par les props
+  getProducts                 // A l'appel, de this.props.userLogin -> isUserLogged  = true
+}
+
+
+export default connect( 
+    mapStateToProps, mapDispatchToProps
+  )(withRouter(ProductPage));
