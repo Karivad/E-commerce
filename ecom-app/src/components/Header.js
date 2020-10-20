3424fc3105892555716aad9232e6fd64319e5abd
Arthur <morinarthurpro@gmail.com>
2020-10-20 15:30:25 +0200

    fin V1

--- ecom-app/src/components/Header.js
+++ ecom-app/src/components/Header.js
@@ -6,13 +6,13 @@ import ProductList from "./ProductList";
 import ProductPage from "./ProductPage";
 import CreateProduct from "./CreateProduct";
 import { connect } from 'react-redux';
-import {userSignOut} from '../actions/userLogin'   // sign in et sign out dans le "userLogin.js"
-import {removeProducts} from '../actions/productsActions'
+import { userSignOut } from '../actions/userLogin'   // sign in et sign out dans le "userLogin.js"
+import { removeProducts } from '../actions/productsActions'
 import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image'
 import Col from 'react-bootstrap/Col'
 
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class Header extends React.Component {
   constructor() {
@@ -37,77 +37,65 @@ class Header extends React.Component {
               </Nav.Link>
             </Nav.Item>
 
-            {!this.props.isLogged.token.length &&
-
-              <p>
+            {!this.props.isLogged.token.length && (
+              <div>
                 <Nav.Item>
-                <Nav.Link href="/sign-up">Sign-up</Nav.Link>
-              </Nav.Item>
+                  <Nav.Link as={Link} to={"/sign-up"}>Sign-up</Nav.Link>
+                </Nav.Item>
 
-              <Nav.Item>
-                <Nav.Link href="/">Sign-in</Nav.Link>
-              </Nav.Item>
-              </p>
-            
-            }
-
+                <Nav.Item>
+                  <Nav.Link as={Link} to={"/"}>Sign-in</Nav.Link>
+                </Nav.Item>
+              </div>
+            )}
             {this.props.isLogged.token.length &&
-            <p>
-              <Nav.Item>
-                <Nav.Link href="/products">Products</Nav.Link>
-              </Nav.Item>
+              <div>
+                <Nav.Item>
+                  <Nav.Link as={Link} to={"/products"}>Products</Nav.Link>
+                </Nav.Item>
 
-              <Nav.Item>
-                <Nav.Link href="/">Create product</Nav.Link>
-              </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to={"/create-product"}>Create product</Nav.Link>
+                </Nav.Item>
 
-              <Nav.Item>
-              
-          
-        <Col xs={2} md={1}>
-            < Image src={ require('../image/pistol.jpeg') } alt="mamie" height={45} width={45} roundedCircle />         
-          </Col>
-        
-        
-          </Nav.Item>
+                <Nav.Item>
 
-              <Nav.Item>
-              <Nav.Link href="/sign-in">
-                <Button 
-                variant="outline-danger" onClick={this.resetStore}>Sign out</Button>{' '}
-                </Nav.Link>
-              </Nav.Item>
-              </p>
-        }
+
+                  <Col xs={2} md={1}>
+                    < Image src={require('../image/pistol.jpeg')} alt="mamie" height={45} width={45} roundedCircle />
+                  </Col>
+
+
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link as={Link} to={"/"}>
+                    <Button
+                      variant="outline-danger" onClick={this.resetStore}>Sign out</Button>{' '}
+                  </Nav.Link>
+                </Nav.Item>
+                </div>
+            }
           </Nav>
 
-                                             {/* import */}
-        {!this.props.isLogged.token.length &&                                  
-          <Switch>
-            <Route path="/">
-              <SignIn />
-            </Route>
-            <Route exact path="/sign-up">
-              <SignUp />
+            <Switch>
+              <Route exact path="/">
+                <SignIn />
               </Route>
-          </Switch>
-        }
-        {this.props.isLogged.token.length &&
-
-          <Switch>
-            <Route  path="/products">
-              < ProductList />
-            </Route>
-
-            
-            <Route exact path="/">
-              <CreateProduct />
-            </Route>
-            <Route path={`/product/:id`} >
-              <ProductPage />
+              <Route exact path="/sign-up">
+                <SignUp />
+              </Route>              
+              <Route path="/products">
+                < ProductList />
               </Route>
-          </Switch>
-          }
+              <Route exact path="/create-product">
+                <CreateProduct />
+              </Route>
+              <Route path={`/product/:id`} >
+                <ProductPage />
+              </Route>
+            </Switch>
+
         </Router>
       </div>
 
@@ -117,7 +105,7 @@ class Header extends React.Component {
 
 function mapStateToProps(state) { //Accéder aux données de notre store dans les props
   return {
-      isLogged: state.isLogged
+    isLogged: state.isLogged
   };
 }
 
@@ -126,6 +114,6 @@ const mapDispatchToProps = { //Permettre de modifier les données par l'appel de
   removeProducts                // A l'appel, de this.props.userLogin -> isUserLogged  = true
 }
 
-export default connect( 
+export default connect(
   mapStateToProps, mapDispatchToProps
 )(Header);
--- ecom-app/src/components/SignIn.js
+++ ecom-app/src/components/SignIn.js
@@ -3,96 +3,103 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {userLogin} from '../actions/userLogin'
+import { userLogin } from '../actions/userLogin'
+import { Link } from "react-router-dom";
+
 const jwt = require('jsonwebtoken')
 
 
 
+
+
 class SignIn extends Component {
-    constructor() {
-        super();
-        this.state = {
-            // name: '',
-            email: '',
-            password: '',
-          
-      }
+  constructor() {
+    super();
+    this.state = {
+      // name: '',
+      email: '',
+      password: '',
+
+    }
+  }
+
+  handleChange = event => {
+    const targetName = event.target.name
+
+    this.setState({ [targetName]: event.target.value })
+
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
     }
 
-    handleChange = event => {
-        const targetName = event.target.name
+    axios.post(`http://localhost:8080/sign-in`, user)
+      .then(res => {
+        const token = res.data.token
+        try {
+          let decoded = jwt.decode(token)
+          decoded.token = token
+          console.log(decoded)
+          this.changeIsLogged(decoded)
+        }
+        catch (err) {
+          console.log(err)
+        }
 
-        this.setState({[targetName]: event.target.value})  
-        
-    }
+      })
+      .catch((err) => {
+        console.log("password ou email erroné");
+      })
+  }
+  changeIsLogged = (data) => {
+    this.props.userLogin(data)
+  }
 
-    handleSubmit = event => {
-        event.preventDefault();
-        const user = {
-            email: this.state.email,
-            password: this.state.password,
-        } 
-      
-        axios.post(`http://localhost:8080/sign-in`, user)
-            .then(res => {
-              const token = res.data.token
-             try {
-              let decoded = jwt.decode(token)
-              decoded.token = token
-              console.log(decoded)
-              this.changeIsLogged(decoded)
-            } 
-            catch (err) {
-              console.log(err)
-            }
-            
-            })
-            .catch((err) => {
-                console.log("password ou email erroné");
-            })
-    }
-    changeIsLogged = (data) => {
-      this.props.userLogin(data)
-    }
+  render() {
+    return (
+      <div>
+        <h1>Sign In</h1>
 
-    render() {
-        return (
-            <div>
-                <h1>Sign In</h1>
-
-<Form onSubmit={this.handleSubmit}>
-    {/* <Form.Group controlId="formBasicName">
+        <Form onSubmit={this.handleSubmit}>
+          {/* <Form.Group controlId="formBasicName">
     <Form.Label>Name</Form.Label>
     <Form.Control type="text" placeholder="Name" name="name" onChange={this.handleChange} />
   </Form.Group> */}
-                    
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChange} />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChange} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
     </Form.Text>
-  </Form.Group>
+          </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChange} />
-  </Form.Group>
-
-  <Button variant="warning" type="submit">
-    Sign In
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChange} />
+          </Form.Group>
+ 
+          <Button variant="warning" type="submit">
+          <Link to={"/products"}>
+            Sign In
+            </Link>
   </Button>
-</Form>
-{this.props.isLogged.isUserLogged && <h2>You are logged</h2>}
+  
+        </Form>
+        {this.props.isLogged.isUserLogged && <h2>You are logged</h2>}
 
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) { //Accéder aux données de notre store dans les props
   return {
-      isLogged: state.isLogged
+    isLogged: state.isLogged
   };
 }
 
@@ -100,6 +107,6 @@ const mapDispatchToProps = { //Permettre de modifier les données par l'appel de
   userLogin                 // A l'appel, de this.props.userLogin -> isUserLogged  = true
 }
 
-export default connect( 
+export default connect(
   mapStateToProps, mapDispatchToProps
 )(SignIn);
