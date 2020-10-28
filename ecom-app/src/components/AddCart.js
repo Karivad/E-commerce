332aff8c381d40c3aa9295171b17c6987f946f19
Arthur <morinarthurpro@gmail.com>
2020-10-28 17:08:31 +0100

    add-cart

--- ecom-app/src/components/AddCart.js
+++ ecom-app/src/components/AddCart.js
@@ -5,28 +5,45 @@ import Table from 'react-bootstrap/Table'
 import { connect } from 'react-redux';
 
 class AddCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: []
+    }
+  }
+
+  displayCart() {
+    let productArray = []
+    this.props.cart.forEach(element => {
+      productArray.push(this.props.products.find(elem => elem.id === element))
+      console.log(productArray)
+      
+    });
+  }
     render() {
         return (
             <div>
                 <h1>Cart</h1>
 
-                <Table striped bordered hover>
+                {/* <Table striped bordered hover>
   <thead>
     <tr>
       <th>Name</th>
       <th>Short description </th>
-      {/* <th>Category </th> */}
+      <th>Category </th>
       <th>Picture</th>
       <th>Price</th>
       <th>Actions</th>
     </tr>
   </thead>
-  <tbody>
-  {this.props.products.filter(element => element.user_affiliate_id === this.props.id).map((tab) => (
+
+  <tbody> */
+        
+  /* {this.props.products.filter(element => element.id === this.props.id).map((tab) => (
     <tr key={tab.id}>
       <td>{tab.titre}</td>
       <td>{tab.description}</td>
-      {/* <td>{tab.categorie}</td> */}
+      <td>{tab.categorie}</td>
       <td><img alt={tab.id} src={tab.image}/></td>
       <td>{tab.prix}</td>
       <td>
@@ -37,11 +54,13 @@ class AddCart extends Component {
       </td>
     </tr>
   )
-  )}
+  )} 
   </tbody>
-</Table>
+</Table>*/}
 
-            </div>
+{this.displayCart()}
+</div>
+            
         );
     }
 }
