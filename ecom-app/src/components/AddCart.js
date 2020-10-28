daf5c19d1d7803d20272d42e40d289c65bc41c2f
Karivad <58565482+Karivad@users.noreply.github.com>
2020-10-28 10:57:21 +0100

    addcart

---
+++ ecom-app/src/components/AddCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddCart extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add to cart</h1>
+            </div>
+        );
+    }
+}
+
+export default AddCart;
